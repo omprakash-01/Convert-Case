403e85178a198f36b4d5010bf24d393a496d218b
omprakash-01 <shintuyadav3@gmail.com>
2024-03-04 18:56:58 +0530

    Project Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
+import Textarea from './components/Textarea'
 
 function App() {
-  
+  let Title="Simply enter your text and choose the case you want to convert it to.";
   return (
     <>
       {/* Navbar */}
-     <Navbar></Navbar>
+     <Navbar></Navbar> 
+     {/* Textarea */}
+     <Textarea Title={Title}></Textarea>
+
     </>
   )
 }
---
+++ src/components/Textarea.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import "./Textarea.css"; // Import the custom CSS file
+
+function Textarea(props) {
+  let [text, setText] = useState("");
+
+  let handlelowercase = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  };
+
+  let handleUPPERCASE = () => {
+    setText(text.toUpperCase());
+  };
+
+  function capitalizeFirstLetter(sentence) {
+    let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+  }
+
+  let handleCapitilazedCase = () => {
+    let newtext = capitalizeFirstLetter(text);
+    setText(newtext);
+  };
+
+  let handleClearText = () => {
+    setText("");
+  };
+
+  let handleonChange = (e) => {
+    let newtext = e.target.value;
+    setText(newtext);
+  };
+
+  return (
+    <>
+      <div>
+        <div className="btn bg-light-subtle">
+          <div className="mt-5">
+            <label
+              htmlFor="exampleFormControlTextarea1"
+              className="form-label mb-4 fs-3"
+            >
+              {props.Title}
+            </label>
+            <textarea
+              className="form-control bg-dark text-white"
+              id="exampleFormControlTextarea1"
+              rows="8"
+              onChange={handleonChange}
+              value={text}
+            ></textarea>
+          </div>
+        </div>
+        <div className="mt-5 ">
+          <button
+            className="btn fs-5 mx-3 bg-secondary text-white p-2 fw-bold custom-btn"
+            onClick={handlelowercase}
+          >
+            lower case
+          </button>
+          <button
+            className="btn fs-5 mx-3 bg-secondary text-white p-2 fw-bold custom-btn"
+            onClick={handleUPPERCASE}
+          >
+            UPPER CASE
+          </button>
+          <button
+            className="btn mx-3 fs-5 bg-secondary text-white p-2 fw-bold custom-btn"
+            onClick={handleCapitilazedCase}
+          >
+            Capitalized Case
+          </button>
+          <button
+            className="btn mx-3 fs-5 bg-secondary text-white p-2 fw-bold custom-btn"
+            onClick={handleClearText}
+          >
+            Clear Text
+          </button>
+        </div>
+        <div className="mt-5">
+          <p className="fw-bold">
+            <span>Character Count: {text.length} | </span>{" "}
+            <span>Word Count:{text.split("").length } | </span>
+            <span>Line Count: {text.split("\n").length-1}</span>
+          </p>
+        </div>
+        <h3 className="fw-bold text-danger ">Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
+
+export default Textarea;
